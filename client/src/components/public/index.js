7002fead72b07413265a3245ce5bd0b8d40495fa
Melissa Murphy <melissa@thoughtfulmonkey.design>
2020-02-06 10:22:00 -0800

    view base

--- client/src/components/public/index.js
+++ client/src/components/public/index.js
@@ -3,15 +3,18 @@ import Header from './headers'
 import RegisterForm from './forms/RegisterForm'
 import LoginForm from './forms/LoginForm'
 import {Route,Switch} from 'react-router-dom'
+import {LombdaLandUI} from './lombdaLandUI'
 const PublicMainView = (props) =>{
 
     return(
         <>
-        <Header />
+        {/* <Header />
         <Switch>
             <Route match path="/register" render={()=><RegisterForm {...props}/>} />
             <Route match pate="/login" render={()=><LoginForm {...props} />} />
-        </Switch>
+        </Switch> */}
+        {/* <p>testing</p> */}
+        <LombdaLandUI />
         </>
     )
 }
---
+++ client/src/components/public/lombdaLandUI/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+// import styled from "styled-components"
+
+export const LombdaLandUI = props => {
+  return (
+    <>
+    <div>header w/ nav once api running</div>
+      <div className="container">test</div>
+    </>
+  )
+}
+
+// const MainView
