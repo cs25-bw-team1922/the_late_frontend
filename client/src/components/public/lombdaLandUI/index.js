8f195988733cbff251dd445cc9d25794d7c51b8f
Melissa Murphy <melissa@thoughtfulmonkey.design>
2020-02-06 10:40:19 -0800

    viewport in prog

--- client/src/components/public/lombdaLandUI/index.js
+++ client/src/components/public/lombdaLandUI/index.js
@@ -1,13 +1,30 @@
 import React from "react"
-// import styled from "styled-components"
+import styled from "styled-components"
 
 export const LombdaLandUI = props => {
   return (
     <>
-    <div>header w/ nav once api running</div>
-      <div className="container">test</div>
+      <div>header w/ nav once api running</div>
+      <div className="container">
+        <MainView>
+          <MapPortal />
+        </MainView>
+      </div>
     </>
   )
 }
 
-// const MainView
+const MainView = styled.div`
+  height: 90vh;
+  width: 90vw;
+  background-color: black;
+  margin: 0 auto;
+`
+const MapPortal = styled.div`
+  height: 40vh;
+  width: 80vw;
+  background-color: white;
+  position: absolute;
+  top: 4rem;
+  left: 10vw;
+`
