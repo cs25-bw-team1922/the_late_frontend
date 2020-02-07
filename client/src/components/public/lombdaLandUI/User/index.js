6951f6333afb97811f658e0a310aeb993b37d2cb
Melissa Murphy <melissa@thoughtfulmonkey.design>
2020-02-06 17:09:22 -0800

    mapping dummy data

--- client/src/components/public/lombdaLandUI/User/index.js
+++ client/src/components/public/lombdaLandUI/User/index.js
@@ -2,7 +2,13 @@ import React from "react"
 import styled from "styled-components"
 
 const User = props => {
-  return <>User Stats</>
+  return (
+    <>
+      <div className="level" user={props.users}>
+        <p>Character Name: {props.user_id}</p>
+      </div>
+    </>
+  )
 }
 
 export default User
--- client/src/components/public/lombdaLandUI/index.js
+++ client/src/components/public/lombdaLandUI/index.js
@@ -6,7 +6,51 @@ import Map from "./Map"
 import User from "./User"
 
 // dummy data
-
+const rooms = [
+  {
+    id: 0,
+    room_name: "Entrance",
+    room_description:
+      "looks like you'll be walking through a ivy covered doorway",
+    room_entrance: "north",
+    room_exit: "north"
+  }
+]
+const items = [
+  {
+    item_id: 0,
+    item_name: "book",
+    item_description: "an old dusty hardback",
+    item_hacker_level: 5,
+    item_health: 3.0,
+    item_power: 1,
+    item_damage: 1.0
+  }
+]
+const users = [
+  {
+    user_id: 0,
+    first_name: "Tommy",
+    last_name: "Thompson",
+    user_name: "TThomp23",
+    password: "pass123",
+    email: "tthomp23@gmail.com",
+    characters: [
+      {
+        char_id: 0,
+        char_name: "Arthur",
+        char_description: "The Knight",
+        char_action: "dash",
+        char_direction: "north",
+        char_speaks: "I am the Knight!",
+        char_hacker_rank: 5,
+        char_health: 3.0,
+        char_lives: 3,
+        char_save_stats: "TRUE"
+      }
+    ]
+  }
+]
 export const LombdaLandUI = props => {
   return (
     <>
@@ -14,12 +58,22 @@ export const LombdaLandUI = props => {
       <div className="container">
         <MainView>
           <div className="level">lombda land</div>
-          <div class="level">
+          <div className="level">
             <MapPortalWrapper>
               <Map />
             </MapPortalWrapper>
             <UserStatsWrapper>
-              <User />
+              {users.map(user => {
+                return (
+                  <User
+                    key={user.user_id}
+                    char_name={user.characters.char_name}
+                    char_description={user.characters.char_description}
+                    char_health={user.characters.char_health}
+                    char_hacker_rank={user.characters.char_hacker_rank}
+                  />
+                )
+              })}
             </UserStatsWrapper>
           </div>
           <HeadsUpWrapper>
